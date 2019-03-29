11106da129973c5cf6041ec15e0049a37c17a086
Deniska728 <denis790504790@gmail.com>
2019-03-30 02:07:11 +0300

    Add auth, Update models, resolvers

--- hackernews-server/src/index.js
+++ hackernews-server/src/index.js
@@ -1,44 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-  },
-  {
-    id: 'link-1',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-]
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))

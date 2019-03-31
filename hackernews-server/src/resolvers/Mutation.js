c41b7424af01967eba9330ea10e3d4404934b315
Deniska728 <denis790504790@gmail.com>
2019-03-31 23:50:54 +0300

    Update resolvers, Add subscriptions, filters, paginations, Complete back-tutorial

--- hackernews-server/src/resolvers/Mutation.js
+++ hackernews-server/src/resolvers/Mutation.js
@@ -34,6 +34,7 @@ async function login(parent, args, context, info) {
 
 function post(parent, args, context, info) {
   const userId = getUserId(context)
+  
   return context.prisma.createLink({
     url: args.url,
     description: args.description,
@@ -41,8 +42,26 @@ function post(parent, args, context, info) {
   })
 }
 
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context)
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId },
+  })
+
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } },
+  })
+}
+
 module.exports = {
   signup,
   login,
   post,
+  vote
 }
